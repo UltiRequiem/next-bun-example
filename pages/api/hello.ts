78a4764ba17785dd5699dbb98f711e73591bf3db
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-08-03 16:17:14 -0500

    :rocket: deploy

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,4 +1,4 @@
-import { NextApiResponse, NextApiRequest } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   return res.status(200).json([1, 2, 3, 4, 5]);
